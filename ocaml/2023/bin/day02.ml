2727b2b4569f92a86ef391f1a3e80631aa88cfe4
Mark Todisco <marktodisco@gmail.com>
2024-11-17 19:36:28 -0500

    added cube color type

--- ocaml/2023/bin/day02.ml
+++ ocaml/2023/bin/day02.ml
@@ -2,7 +2,24 @@ open Core
 open Printf
 
 exception No_lines of string
-exception RuntimeError of string
+
+type cube_count =
+  { red : int
+  ; green : int
+  ; blue : int
+  }
+
+let update_color cubes color count =
+  match color with
+  | "red" -> { cubes with red = count }
+  | "green" -> { cubes with green = count }
+  | "blue" -> { cubes with blue = count }
+  | _ -> failwith "Invalid color"
+;;
+
+let print_rbg rbg =
+  Printf.printf "{ red = %i; green = %i; blue = %i; }\n" rbg.red rbg.green rbg.blue
+;;
 
 let read_lines (path : string) : string list =
   let ic = In_channel.create path in
@@ -44,24 +61,45 @@ let parse_game_id (line : string) : int =
 
 (* let parse_game_sets (line : string) : (int * int * int) list = *)
 let parse_game (line : string) : string list =
-  let game =
+  (* Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green *)
+  let game_text =
     match String.split line ~on:':' with
     | [ _; right ] -> right
-    | _ -> raise (RuntimeError "Could not split line on ':'")
+    | _ -> failwith "Could not split line on ':'"
   in
+  print_endline ("game_text: " ^ game_text);
   let game_sets =
-    match String.split game ~on:';' with
-    | [] -> raise (RuntimeError "This game had zero sets")
+    match String.split game_text ~on:';' with
+    | [] -> failwith "This game had zero sets"
     | sets -> sets
   in
-  printf "text = %s\n" game;
-  let game =
-    match List.nth game_sets 0 with
-    | Some x -> x
-    | None -> failwith "No games"
+  printf "text = %s\n" game_text;
+  let game_text =
+    (match List.nth game_sets 0 with
+     | Some x -> x
+     | None -> failwith "No games")
+    |> String.strip
   in
-  print_endline ("game: " ^ game);
-  let games = List.map game_sets ~f:(fun x -> String.split x ~on:',') in
+  let rolls = List.map (String.split game_text ~on:',') ~f:String.strip in
+  List.iter rolls ~f:print_endline;
+  let roll =
+    match List.nth rolls 0 with
+    | Some x -> x
+    | None -> failwith "No rolls"
+  in
+  print_endline ("roll: " ^ roll);
+  let count, color =
+    match String.split roll ~on:' ' with
+    | [ count; color ] -> int_of_string count, color
+    | _ -> failwith "Invalid count and color"
+  in
+  let cubes : cube_count = { red = 0; green = 0; blue = 0 } in
+  print_rbg cubes;
+  let cubes = update_color cubes color count in
+  print_rbg cubes;
+  printf "%s: " color;
+  printf "%i\n" count;
+  (* let games = List.map game_sets ~f:(fun x -> String.split x ~on:',') in *)
   game_sets
 ;;
 
@@ -74,8 +112,7 @@ let () =
     | None -> raise (No_lines "No lines found in source")
   in
   let game_id = parse_game_id line in
-  let games = parse_game line in
-  List.iter games ~f:(fun x -> printf "  %s\n" x);
+  let _ = parse_game line in
   printf "==========\n";
   printf "game_id = %i\n" game_id;
   printf "==========\n"
