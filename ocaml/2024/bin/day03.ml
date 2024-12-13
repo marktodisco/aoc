1a80fa4d97e15e2b488668fb276fbedd2b186efb
Mark Todisco <marktodisco@gmail.com>
2024-12-13 12:49:50 -0500

    created submodules in Advent

--- ocaml/2024/bin/day03.ml
+++ ocaml/2024/bin/day03.ml
@@ -2,14 +2,14 @@ open Printf
 
 let () =
   (* let lines = [ "mul(1,2)mul[3,4]don't()mul(6,7)do()mul(100,1000)" ] in *)
-  let lines = "./data/d3.txt" |> Advent.read_lines in
-  let matches = lines |> List.map Advent.extract_groups |> List.flatten in
-  let valid_matches = matches |> Advent.filter_mul_groups in
+  let lines = "./data/d3.txt" |> Advent.IO.read_lines in
+  let matches = lines |> List.map Advent.Regex.extract_groups |> List.flatten in
+  let valid_matches = matches |> Advent.Regex.filter_mul_groups in
   let total =
     valid_matches
     |> List.map (String.split_on_char ',')
     |> List.map (fun xs -> List.map int_of_string xs)
-    |> List.map Advent.product
+    |> List.map Advent.Math.product
     |> List.fold_left ( + ) 0
   in
   printf "\nmul: %i\n" total;
--- ocaml/2024/lib/advent.ml
+++ ocaml/2024/lib/advent.ml
@@ -1,62 +1,74 @@
 open Printf
 
-let read_lines (path : string) : string list =
-  let ic = Core.In_channel.create path in
-  let contents = Core.In_channel.input_lines ic in
-  In_channel.close ic;
-  contents
-;;
+module IO = struct
+  let read_lines (path : string) : string list =
+    let ic = Core.In_channel.create path in
+    let contents = Core.In_channel.input_lines ic in
+    In_channel.close ic;
+    contents
+  ;;
 
-let print_list ?(prefix = "") ?(postfix = "\n") printer lst =
-  printf "%s" prefix;
-  printf "[";
-  Core.List.iteri
-    ~f:(fun i x ->
-      if i > 0 then printf "; ";
-      printer x)
-    lst;
-  printf "]";
-  printf "%s" postfix
-;;
+  let print_list ?(prefix = "") ?(postfix = "\n") printer lst =
+    printf "%s" prefix;
+    printf "[";
+    Core.List.iteri
+      ~f:(fun i x ->
+        if i > 0 then printf "; ";
+        printer x)
+      lst;
+    printf "]";
+    printf "%s" postfix
+  ;;
 
-let int_printer = printf "%d"
-let bool_printer = printf "%b"
-let string_printer = printf "\"%s\""
-let print_hashtbl h = Stdlib.Hashtbl.iter (fun k v -> printf "%i => %i\n" k v) h
+  let int_printer = printf "%d"
+  let bool_printer = printf "%b"
+  let string_printer = printf "\"%s\""
+  let print_hashtbl h = Stdlib.Hashtbl.iter (fun k v -> printf "%i => %i\n" k v) h
+end
 
-let dropi items i =
-  if i < 0 || i >= List.length items then failwith ("invalid index: " ^ string_of_int i);
-  Core.List.filteri ~f:(fun i' _ -> i' <> i) items
-;;
+module List = struct
+  let dropi items i =
+    if i < 0 || i >= List.length items then failwith ("invalid index: " ^ string_of_int i);
+    Core.List.filteri ~f:(fun i' _ -> i' <> i) items
+  ;;
+end
 
-let extract_groups text =
-  let mul_pattern =
-    Re.seq
-      [ Re.str "mul("
-      ; Re.group (Re.seq [ Re.rep1 Re.digit; Re.str ","; Re.rep1 Re.digit ])
-      ; Re.str ")"
-      ]
-  in
-  let do_pattern = Re.group (Re.str "do()") in
-  let dont_pattern = Re.group (Re.str "don't()") in
-  let regex = Re.compile (Re.alt [ mul_pattern; do_pattern; dont_pattern ]) in
-  let matches = Re.all regex text in
-  List.map
-    (fun g ->
-      Re.Group.all g |> Array.to_list |> List.filteri (fun i s -> i > 0 && s <> ""))
-    matches
-  |> List.flatten
-;;
+module Regex = struct
+  open Stdlib
 
-let filter_mul_groups groups =
-  let rec aux enabled groups' acc =
-    match groups' with
-    | "do()" :: tail -> aux true tail acc
-    | "don't()" :: tail -> aux false tail acc
-    | g :: tail -> aux enabled tail (if enabled then g :: acc else acc)
-    | [] -> acc
-  in
-  List.rev (aux true groups [])
-;;
+  let extract_groups text =
+    let mul_pattern =
+      Re.seq
+        [ Re.str "mul("
+        ; Re.group (Re.seq [ Re.rep1 Re.digit; Re.str ","; Re.rep1 Re.digit ])
+        ; Re.str ")"
+        ]
+    in
+    let do_pattern = Re.group (Re.str "do()") in
+    let dont_pattern = Re.group (Re.str "don't()") in
+    let regex = Re.compile (Re.alt [ mul_pattern; do_pattern; dont_pattern ]) in
+    let matches = Re.all regex text in
+    List.map
+      (fun g ->
+        Re.Group.all g |> Array.to_list |> List.filteri (fun i s -> i > 0 && s <> ""))
+      matches
+    |> List.flatten
+  ;;
 
-let product nums = List.fold_left Int.mul 1 nums
+  let filter_mul_groups groups =
+    let rec aux enabled groups' acc =
+      match groups' with
+      | "do()" :: tail -> aux true tail acc
+      | "don't()" :: tail -> aux false tail acc
+      | g :: tail -> aux enabled tail (if enabled then g :: acc else acc)
+      | [] -> acc
+    in
+    List.rev (aux true groups [])
+  ;;
+end
+
+module Math = struct
+  open Stdlib
+
+  let product nums = List.fold_left Int.mul 1 nums
+end
